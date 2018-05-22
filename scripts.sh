35e00e24eac2ff2750060be860a43a67fe58bc90
rohkum143 <rohtashkumar98@yahoo.com>
2018-05-22 18:49:43 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
-if git rev-parse ${TAG} >/dev/null 2>&1
+if git rev-parse $1 >/dev/null 2>&1
 then
-    echo "Found ${TAG}"
+    echo "Found $1"
     exit 1
 else
-    echo "create tag for ${TAG}"
-    git tag ${TAG}
+    echo "create tag for $1"
+    git tag $1
     // git push origin test4
 fi
